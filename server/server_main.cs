e1123791a1d588bec536a7d44fefb9413f9668a8
peter <2021pcostesc@tjhsst.edu>
2020-12-12 15:20:24 -0500

    working website; 2 text boxes

--- server/server_main.cs
+++ server/server_main.cs
@@ -1,1 +1,40 @@
 // trying to set up the branches correctly
+using System;
+using System.Net;
+using System.IO;
+
+namespace server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sURL;
+            sURL = "http://www.google.com";
+
+            WebRequest wrGETURL;
+            wrGETURL = WebRequest.Create(sURL);
+
+            WebProxy myProxy = new WebProxy("myproxy", 80);
+            myProxy.BypassProxyOnLocal = true;
+
+            wrGETURL.Proxy = WebProxy.GetDefaultProxy();
+
+            Stream objStream;
+            objStream = wrGETURL.GetResponse().GetResponseStream();
+
+            StreamReader objReader = new StreamReader(objStream);
+
+            string sLine = "";
+            int i = 0;
+
+            while (sLine != null)
+            {
+                i++;
+                sLine = objReader.ReadLine();
+                if (sLine != null)
+                    Console.WriteLine("{0}:{1}", i, sLine);
+            }
+            Console.ReadLine();        }
+    }
+}
