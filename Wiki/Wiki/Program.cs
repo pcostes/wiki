90828a2ec9be60f5d91e7965f02b6c8adb263f0c
peter <2021pcostesc@tjhsst.edu>
2020-12-12 17:49:45 -0500

    working link slicer

--- Wiki/Wiki/Program.cs
+++ Wiki/Wiki/Program.cs
@@ -2,9 +2,14 @@
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Collections.Generic;
-
+using Newtonsoft.Json;
 namespace C__Folder
 {
+    public class Link
+	{
+        public int ns;
+        public string title;
+    }
     class Program
     {
         static async Task Main(string[] args)
@@ -14,6 +19,7 @@ namespace C__Folder
             Console.WriteLine("done");
             Console.ReadKey();
         }
+
         async static Task<string[]> bfs(string root, string target)
         {
             return null;
@@ -24,13 +30,19 @@ namespace C__Folder
             {
                 //string full url = "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=Craig%20Noone&format=json"
                 //string fullUrl = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=iwlinks&titles=Craig%20Noone";
-                string fullUrl = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&links=plnamespace&titles=Burt,%20County%20Donegal";
+                string fullUrl = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=links&titles=Burt,%20County%20Donegal";
                 using (HttpResponseMessage response = await client.GetAsync(fullUrl))
                 {
                     using (HttpContent content = response.Content)
                     {
                         string mycontent = await content.ReadAsStringAsync();
                         Console.WriteLine(mycontent);
+                        string key = "\"links\":";
+                        int start_pos = mycontent.IndexOf(key) + key.Length;
+                        string links = mycontent.Substring(start_pos, mycontent.IndexOf("]") + 1 - start_pos);
+                        List<Link> generics = JsonConvert.DeserializeObject<List<Link>>(links);
+
+                        Console.WriteLine(generics);
                         Console.WriteLine("done (ineer)");
                     }
                 }
