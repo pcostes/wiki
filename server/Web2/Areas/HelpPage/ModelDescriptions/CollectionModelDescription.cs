a39d6b91421046ee13b6a0e6bf3b8197fb36a9e5
peter <2021pcostesc@tjhsst.edu>
2020-12-13 11:48:42 -0500

    trying to fix .gitignore

--- server/Web2/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace Web2.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
