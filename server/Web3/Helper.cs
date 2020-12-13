5dee0f95dc40a074025db0871bb5fe5ff171ba46
peter <2021pcostesc@tjhsst.edu>
2020-12-13 11:49:44 -0500

    fixed gitignore. Webserver hosted correctly.

---
+++ server/Web3/Helper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web3
+{
+	public class Helper
+	{
+		public static string[] dummyBFS(string start, string end)
+		{
+			// make some api calls
+			double i = 0.0;
+			while (true)
+			{
+				i += 1.4;
+				i /= 2.45;
+				i -= 5.1;
+				i *= 42314;
+
+			}
+			return new string[] { start, "middle", end};// + " - " + start;
+		}
+	}
+}
---
+++ server/WikiSolverServer/solver.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace WikiSolverServer
+{
+	public partial class solver : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		protected void Submit_Click1(object sender, EventArgs e)
+		{
+			string[] result = Helper.dummyBFS(StartPoint.Text, EndPoint.Text);
+			NameLabel.Text = "Length: " + result.Length.ToString();
+			System.Data.DataTable data = new DataTable();
+			data.Columns.Add("Path");
+			foreach (var r in result)
+			{
+				data.Rows.Add(r);
+			}
+
+			PathList.DataSource = data;
+			PathList.DataBind();
+		}
+	}
+}
